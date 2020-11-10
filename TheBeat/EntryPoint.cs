f2a6231c86d2103fa579a6c2e343d8d802a5fbc7
Krisztian Zsolczai <krisztianzs@gmail.com>
2020-11-10 18:46:40 -0500

    Added LocationHelper class.

--- TheBeat/EntryPoint.cs
+++ TheBeat/EntryPoint.cs
@@ -8,39 +8,61 @@ using LSPD_First_Response.Mod.API;
 using LSPD_First_Response.Mod.Callouts;
 using Rage;
 
+[assembly: Rage.Attributes.Plugin("The Beat Plugin", Description = "Creates Ambient Events")]
 namespace TheBeat
 {
-    public class EntryPoint : Plugin
+    public static class EntryPoint //: Plugin
     {
-        /// <summary>
-        /// This method is run when the plugin is first initialized.
-        /// </summary>
-        public override void Initialize()
+        public static void Main()
         {
-            //Subscribe to the OnOnDutyStateChanged event, so we don't register our callouts unless the player is on duty.
-            Functions.OnOnDutyStateChanged += this.OnDutyStateChangedEvent;
+            Vector3 SpawnPoint = new Vector3(2325.0f, 3141.0f, 0f);
 
-            //Logging is a great tool, so we log to make sure the plugins loaded.
-            Game.LogTrivial("----> TheBeat initialized");
-        }
-
-        /// <summary>
-        /// Called when the OnOnDutyStateChanged event is raised.        
-        public void OnDutyStateChangedEvent(bool onDuty)
-        {
-            //If the player is going on duty, register the callout.
-            if (onDuty)
+            while (true)
             {
-                
+                if (Helpers.LocationHelper.PlayerWithinLocation(50f, SpawnPoint))
+                {
+                    Game.DisplaySubtitle("Player entered location");
+                }
+                else
+                {
+                    Game.DisplaySubtitle("Player exited location");
+                }
+                Vector3 playerLocation = Game.LocalPlayer.Character.Position;
+                //Game.DisplaySubtitle("location: "  + playerLocation);
+                Rage.GameFiber.Yield();
             }
+
         }
 
-        /// <summary>
-        /// Called before the plugin is unloaded.
-        /// </summary>
-        public override void Finally()
-        {
-            Game.LogTrivial("----> TheBeat unloaded");
-        }
+        ///// <summary>
+        ///// This method is run when the plugin is first initialized.
+        ///// </summary>
+        //public override void Initialize()
+        //{
+        //    //Subscribe to the OnOnDutyStateChanged event, so we don't register our callouts unless the player is on duty.
+        //    Functions.OnOnDutyStateChanged += this.OnDutyStateChangedEvent;
+
+        //    //Logging is a great tool, so we log to make sure the plugins loaded.
+        //    Game.LogTrivial("----> TheBeat initialized");
+        //}
+
+        ///// <summary>
+        ///// Called when the OnOnDutyStateChanged event is raised.        
+        //public void OnDutyStateChangedEvent(bool onDuty)
+        //{
+        //    //If the player is going on duty, register the callout.
+        //    if (onDuty)
+        //    {
+                
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Called before the plugin is unloaded.
+        ///// </summary>
+        //public override void Finally()
+        //{
+        //    Game.LogTrivial("----> TheBeat unloaded");
+        //}
     }
 }
---
+++ TheBeat/Helpers/LocationHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+
+namespace TheBeat.Helpers
+{
+    class LocationHelper
+    {
+        public static bool PlayerWithinLocation(float distanceFromLocation, Vector3 location)
+        {
+            if (Game.LocalPlayer.Character.Position.DistanceTo(location) <= distanceFromLocation)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
